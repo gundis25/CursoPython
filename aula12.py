73a4dd014106650826d6e144af1f1b66f5c64190
Gundis25 <marcosgundis@gmail.com>
2024-07-16 01:49:37 +0000

    Calculo imc

--- aula12.py
+++ aula12.py
@@ -1,5 +1,5 @@
 nome = 'MArcos Antonio'
 altura = 1.80
 peso = 95 
-imc = peso / altura ** 2
+imc = peso / altura ** 2 # Ou imc = peso / (altura * altura)
 print(nome ,' tem ' ,altura,'de altura, pesa ',peso,' quilos e seu IMC e ',imc)
