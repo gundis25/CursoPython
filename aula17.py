3d58c0cdc341f0e621086121281358e79c12daca
Gundis25 <marcosgundis@gmail.com>
2024-07-19 00:22:07 +0000

    if/elif/elseflux/interp

---
+++ aula17.py
@@ -0,0 +1,23 @@
+# if / elif       / else
+# se / se nao se /se nao
+condicional1 = False
+condicional2 = False
+condicional3 = False
+condicional4 = False
+
+if condicional1:
+    print('Ccodigo para condicinal1')
+    print('Ccodigo para condicinal1')
+elif condicional2:
+    print('Codigo para  condicional2')
+elif condicional3:
+    print('Codigo para condicional3')
+elif condicional4:
+    print('Codigo para condicional4')
+else:
+    print('nenhuma condicao foi satisfeita')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do print')
