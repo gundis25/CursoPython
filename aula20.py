a8e98ab7a26dcdeeab724f9206ee8378b1474201
Gundis25 <marcosgundis@gmail.com>
2024-07-23 00:53:55 +0000

    exerc if/comparaçao

---
+++ aula20.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor :
+    print(f'{primeiro_valor=} e maior ou igual ao {segundo_valor=}')
+else :
+    print(f'{segundo_valor=} e maior que o {primeiro_valor=}')
