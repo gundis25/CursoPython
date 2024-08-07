ccfaa94096816a936a68fa0f77b2e235cbec535b
Gundis25 <marcosgundis@gmail.com>
2024-08-07 01:59:04 +0000

    Fatiamento de string

---
+++ aula27.py
@@ -0,0 +1,23 @@
+"""
+Fatiamento de strings
+ 012345678
+ Ola mundo
+-987654321
+Fatiamento [i:f:p] [::]
+obs. : a funçao len retorna a qtd
+de caracteres da str
+"""
+variavel = "Ola mundo"
+print(variavel[4])
+print(variavel[4:])
+print(variavel[4:8])
+print(variavel[:5])
+print(variavel[-8:-2])
+print(variavel[3])
+print(variavel[4])
+print(len(variavel[3]))
+print(len(variavel))
+print(variavel[4])
+print(variavel[0:len(variavel):1])
+print(variavel[0:9:2])
+print(variavel[-1:-10:-1])
