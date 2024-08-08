6a17b7120007a34aad3e5a3a36df74384f9821e0
Gundis25 <marcosgundis@gmail.com>
2024-08-08 01:25:59 +0000

    intr try/except

---
+++ aula29.py
@@ -0,0 +1,22 @@
+"""
+Introduçao ao try/except
+try -> tenta executar o coddigo
+except -> ocorreu algum erro ao tentar executar
+"""
+numero_str = input('Vou dobras o numero que voce digitar:')
+
+try:
+    print('STR:', numero_str)
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'Odobro de {numero_str} e {numero_float * 2:.2f}')
+except:
+    print('Isso nao e um numero')
+"""    
+
+if numero_str.isdigit(): # O numero digitado deve ser inteiro
+    numero_float = float(numero_str)
+    print(f'O dobro de {numero_str} e {numero_float * 2:.1f}')
+else:
+    print('Isso nao e um numero')
+"""
