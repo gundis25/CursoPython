042aefd28d43173269d957e52d9e76ed9b6412e8
Gundis25 <marcosgundis@gmail.com>
2024-08-10 01:06:56 +0000

    id-A identidade que esta na memoria

---
+++ aula31.py
@@ -0,0 +1,17 @@
+"""
+Flag (Bandeira) - Marca um local
+Nome = nao valor
+is e is not = e ou nao e (tipo, valor, identidade)
+id = Identidade (identifica um valor que esta na memoria)
+
+"""
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
+#condiçao = False 
+
+# if condiçao:
+#      print('Faça algo')
+# esle:
+#       print('Nao facac algo')
